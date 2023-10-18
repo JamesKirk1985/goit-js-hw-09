c59ed24ce19df688dcfa07d4a0adb50fdd17508e
JamesKirk1985 <134450334+JamesKirk1985@users.noreply.github.com>
2023-10-18 05:47:24 +0000

    Deploying to gh-pages from @ JamesKirk1985/goit-js-hw-09@41b17bb3e5a29b21be8b6a7e507d975721573099 🚀

---
+++ 01-color-switcher.020cdbf3.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),e=document.querySelector("[data-start]"),n=document.querySelector("[data-stop]");e.addEventListener("click",(function(){t.style.backgroundColor=r(),e.disabled=!0,o=setInterval((()=>{t.style.backgroundColor=r()}),1e3)})),n.addEventListener("click",(function(){clearInterval(o),e.disabled=!1}));let o=null;function r(){return`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}
+//# sourceMappingURL=01-color-switcher.020cdbf3.js.map
---
+++ 01-color-switcher.b7062f9b.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("body"),e=document.querySelector("[data-start]"),n=document.querySelector("[data-stop]");e.addEventListener("click",(function(){t.style.backgroundColor=r(),e.disabled=!0,o=setInterval((function(){t.style.backgroundColor=r()}),1e3)})),n.addEventListener("click",(function(){clearInterval(o),e.disabled=!1}));var o=null;function r(){return"#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}}();
+//# sourceMappingURL=01-color-switcher.b7062f9b.js.map
