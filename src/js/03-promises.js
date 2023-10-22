126d7e4e4401ef9f8856d4026e137809c6d59711
JamesKirk1985 <134450334+JamesKirk1985@users.noreply.github.com>
2023-10-23 00:18:15 +0300

    third finishied

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,36 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+const form = document.querySelector('.form');
+form.addEventListener('submit', clickHandler);
+
+function clickHandler(event) {
+  event.preventDefault();
+  delay = form.elements.delay.value;
+  step = form.elements.step.value;
+  amount = form.elements.amount.value;
+  for (let i = 1; i <= amount; i += 1) {
+    position = i;
+    delay = Number(delay) + i * step;
+    createPromise(position, delay);
   }
+}
+
+function createPromise(position, delay) {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position, delay });
+      } else {
+        // Reject
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+  promise
+    .then(({ position, delay }) => {
+      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
 }
